258ada79f4da78712f4e50c82f516560bae4fca9
Maksym Polinka <Polinka7max@gmail.com>
2020-02-22 07:59:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+
+
+//TODO: make the app run from here --> you have a problem: you need to use yarn to run your React app from here but WebStorm uses npm
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [hello, setHello] = React.useState({
+        hello: ""
+    });
+
+    useEffect(() => { // TODO: get in the concept of promises
+        fetch('http://localhost:5000/',
+            {
+                mode: 'cors',
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then(json => setHello(json))
+    }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        hello {hello.hello}
     </div>
   );
 }
